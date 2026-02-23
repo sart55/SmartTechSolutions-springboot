47ac298c77c6573f3dc84dc9af27e280d6b5ec80
sart55 <105958020+sart55@users.noreply.github.com>
2026-02-24 02:02:56 +0530

    Update SmarttechBackendApplication.java

--- smartech-backend/src/main/java/com/smarttech/smarttech_backend/SmarttechBackendApplication.java
+++ smartech-backend/src/main/java/com/smarttech/smarttech_backend/SmarttechBackendApplication.java
@@ -1,1 +1,13 @@
+package com.smarttech.smarttech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttechBackendApplication.class, args);
+	}
+}
 
