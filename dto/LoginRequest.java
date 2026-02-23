43f8b3c6e1104b9039c4ff16de0f7378813d98f8
sart55 <105958020+sart55@users.noreply.github.com>
2026-02-24 00:52:22 +0530

    Add files via upload

--- dto/LoginRequest.java
+++ dto/LoginRequest.java
@@ -1,11 +1,13 @@
 package com.smarttech.smarttech_backend.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class AuthRequest {
+@Data
+public class LoginRequest {
     private String username;
     private String password;
 }
