01005a35955aad69269c3ff7fb8aa60cef94fd58
sart55 <105958020+sart55@users.noreply.github.com>
2026-02-24 02:15:06 +0530

    Add files via upload

--- smartech-backend/src/main/java/com/smarttech/smarttech_backend/security/AuthResponse.java
+++ smartech-backend/src/main/java/com/smarttech/smarttech_backend/security/AuthResponse.java
@@ -1,1 +1,13 @@
+package com.smarttech.smarttech_backend.security;
 
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
