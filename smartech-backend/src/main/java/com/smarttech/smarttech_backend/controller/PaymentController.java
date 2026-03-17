f2f1b5489eb63c5bc7a8b6028684fd0d086e318d
sart55 <105958020+sart55@users.noreply.github.com>
2026-03-18 01:54:25 +0530

    Update PaymentController.java

--- smartech-backend/src/main/java/com/smarttech/smarttech_backend/controller/PaymentController.java
+++ smartech-backend/src/main/java/com/smarttech/smarttech_backend/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.smarttech.smarttech_backend.controller;
 
+import com.smarttech.smarttech_backend.dto.PaymentSummary;
 import com.smarttech.smarttech_backend.entity.Payment;
+import com.smarttech.smarttech_backend.entity.Project;
 import com.smarttech.smarttech_backend.service.PaymentService;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +27,19 @@ public class PaymentController {
         String username = authentication.getName();
 
         return paymentService.savePayment(projectId, payment, username);
+    }
+
+    @GetMapping("/project/{projectId}/summary")
+    public PaymentSummary getPaymentSummary(@PathVariable Long projectId) {
+        return paymentService.getPaymentSummary(projectId);
+    }
+
+    @PostMapping("/project/{projectId}/total")
+    public Project saveTotalAmount(
+            @PathVariable Long projectId,
+            @RequestBody Project requestBody) {
+
+        return paymentService.saveProjectTotalAmount(projectId, requestBody.getTotalAmount());
     }
 
     @DeleteMapping("/project/{projectId}/all")
